2433a20941d028903634a535c070756f2206b9fd
Ian Mark <ianmarkbuenaflor9@gmail.com>
2026-05-29 15:18:47 +0800

    fix deployment errors

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = process.env.BACKEND_API || process.env.BACKEND_API_LOCAL;
 
 // Create instance
 export const api = axios.create({
